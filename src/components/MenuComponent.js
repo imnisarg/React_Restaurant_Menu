647ac47405f5da9920fc0e739de2e278d73c73c5
imnisarg <enbshah2010@gmail.com>
2020-04-01 23:17:27 +0530

    Initial Commit

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,66 @@
+import React , {Component} from 'react';
+import { Card, CardImg , CardImgOverlay, CardText , CardBody ,CardTitle } from 'reactstrap';
+class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedDish : null
+        };
+    }
+    onDishSelect(dish) {
+        this.setState({selectedDish : dish})
+    } 
+    renderDish(dish) {
+        if(dish!= null)
+        {
+            return(
+            <Card>
+               <CardImg width="100%" src = {dish.image} alt = {dish.name}/>
+               <CardBody>
+                   <CardTitle>{dish.name}</CardTitle>
+                   <CardText>{dish.description}</CardText>
+               </CardBody> 
+            </Card>
+            );
+        }
+        else
+        {
+            return(
+                <div></div>
+            );
+        }
+    }
+    render() {
+        const menu = this.props.dishes.map((dish) => {
+            return(
+                <div key={dish.id} className = "col-12 mt-5">
+                    
+                    <Card onClick={ () => this.onDishSelect(dish) }>
+                        
+                            <CardImg width="100%" src = {dish.image} alt = {dish.name}/>
+
+                        
+                        <CardImgOverlay>
+                            <CardTitle heading>{dish.name}</CardTitle>
+                            
+                        </CardImgOverlay>
+                    </Card>
+
+                </div>
+            );
+        });
+        
+        return (
+            <div ClassName = "Container">
+                <div ClassName = "Row">
+                    {menu}
+                </div>
+                <div ClassName = "Row">
+                    {this.renderDish(this.state.selectedDish)}
+                </div>
+            </div>
+        );
+    }
+
+}
+export default Menu;
